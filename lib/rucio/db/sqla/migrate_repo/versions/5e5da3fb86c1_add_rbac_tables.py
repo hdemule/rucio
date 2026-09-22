8eb70f16cdaeec15a647e41b7df9dc4290a1c56c
Hugo Demule <hugo.jean.roger.demule@cern.ch>
2026-09-22 16:29:18 +0200

    feat(Database): updated alembic migration script

--- lib/rucio/db/sqla/migrate_repo/versions/5e5da3fb86c1_add_rbac_tables.py
+++ lib/rucio/db/sqla/migrate_repo/versions/5e5da3fb86c1_add_rbac_tables.py
@@ -34,42 +34,44 @@ def upgrade():
         create_table('roles',
                      sa.Column('role', sa.String(255)),
                      sa.Column('description', sa.Text),
+                     sa.Column('assignment_disabled', sa.Boolean, default=False),
+                     sa.Column('internal_role_flag', sa.Boolean, default=False),
                      sa.Column('created_at', sa.DateTime, default=datetime.datetime.utcnow),
                      sa.Column('updated_at', sa.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow))
         create_primary_key('ROLES_PK', 'roles', ['role'])
         create_check_constraint('ROLES_ROLE_NN', 'roles', 'role is not null')
+        create_check_constraint('ROLES_ASSIGNMENT_DISABLED_NN', 'roles', 'assignment_disabled is not null')
+        create_check_constraint('ROLES_INTERNAL_ROLE_FLAG_NN', 'roles', 'internal_role_flag is not null')
         create_check_constraint('ROLES_CREATED_NN', 'roles', 'created_at is not null')
         create_check_constraint('ROLES_UPDATED_NN', 'roles', 'updated_at is not null')
 
         create_table('account_role_map',
                      sa.Column('account', sa.String(get_schema_value('ACCOUNT_LENGTH'))),
                      sa.Column('role', sa.String(255)),
-                     sa.Column('locked', sa.Boolean, default=False),
+                     sa.Column('expires_at', sa.DateTime, default=None),
                      sa.Column('created_at', sa.DateTime, default=datetime.datetime.utcnow),
                      sa.Column('updated_at', sa.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow))
         create_primary_key('ACCOUNT_ROLE_MAP_PK', 'account_role_map', ['account', 'role'])
-        create_foreign_key('ACCOUNT_ROLE_MAP_ACCOUNT_FK', 'account_role_map', 'accounts', ['account'], ['account'])
-        create_foreign_key('ACCOUNT_ROLE_MAP_ROLE_FK', 'account_role_map', 'roles', ['role'], ['role'])
+        create_foreign_key('ACCOUNT_ROLE_MAP_ACCOUNT_FK', 'account_role_map', 'accounts', ['account'], ['account'], onupdate='CASCADE', ondelete='CASCADE')
+        create_foreign_key('ACCOUNT_ROLE_MAP_ROLE_FK', 'account_role_map', 'roles', ['role'], ['role'], onupdate='CASCADE', ondelete='RESTRICT')
         create_check_constraint('ACCOUNT_ROLE_MAP_ACCOUNT_NN', 'account_role_map', 'account is not null')
         create_check_constraint('ACCOUNT_ROLE_MAP_ROLE_NN', 'account_role_map', 'role is not null')
-        create_check_constraint('ACCOUNT_ROLE_MAP_LOCKED_NN', 'account_role_map', 'locked is not null')
         create_check_constraint('ACCOUNT_ROLE_MAP_CREATED_NN', 'account_role_map', 'created_at is not null')
         create_check_constraint('ACCOUNT_ROLE_MAP_UPDATED_NN', 'account_role_map', 'updated_at is not null')
 
         create_table('role_permission_map',
                      sa.Column('role', sa.String(255)),
-                     sa.Column('scope', sa.String(get_schema_value('SCOPE_LENGTH'))),
+                     sa.Column('scope_pattern', sa.String(get_schema_value('SCOPE_LENGTH'))),
                      sa.Column('operation', sa.Enum(DatabaseOperationType,
                                                     name='ROLE_PERMISSION_MAP_OPERATION_CHK',
                                                     create_constraint=True,
                                                     values_callable=lambda obj: [e.value for e in obj])),
                      sa.Column('created_at', sa.DateTime, default=datetime.datetime.utcnow),
                      sa.Column('updated_at', sa.DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow))
-        create_primary_key('ROLE_PERMISSION_MAP_PK', 'role_permission_map', ['role', 'scope', 'operation'])
-        create_foreign_key('ROLE_PERMISSION_MAP_ROLE_FK', 'role_permission_map', 'roles', ['role'], ['role'])
-        create_foreign_key('ROLE_PERMISSION_MAP_SCOPE_FK', 'role_permission_map', 'scopes', ['scope'], ['scope'])
+        create_primary_key('ROLE_PERMISSION_MAP_PK', 'role_permission_map', ['role', 'scope_pattern', 'operation'])
+        create_foreign_key('ROLE_PERMISSION_MAP_ROLE_FK', 'role_permission_map', 'roles', ['role'], ['role'], onupdate='CASCADE', ondelete='RESTRICT')
         create_check_constraint('ROLE_PERMISSION_MAP_ROLE_NN', 'role_permission_map', 'role is not null')
-        create_check_constraint('ROLE_PERMISSION_MAP_SCOPE_NN', 'role_permission_map', 'scope is not null')
+        create_check_constraint('ROLE_PERMISSION_MAP_SCOPE_PATTERN_NN', 'role_permission_map', 'scope_pattern is not null')
         create_check_constraint('ROLE_PERMISSION_MAP_OPERATION_NN', 'role_permission_map', 'operation is not null')
         create_check_constraint('ROLE_PERMISSION_MAP_CREATED_NN', 'role_permission_map', 'created_at is not null')
         create_check_constraint('ROLE_PERMISSION_MAP_UPDATED_NN', 'role_permission_map', 'updated_at is not null')
