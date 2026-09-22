1b7e709598f2dacf4e967b179f18fdff59adb32b
Hugo Demule <hugo.jean.roger.demule@cern.ch>
2026-09-22 15:26:59 +0200

    feat(Policies): adapt core role functions to match the new DB schema This includes working with assignment_disabled and internal_role_flag for roles. This also handles new scope patterns instead of scopes directly

--- policy-packages/rbac_rucio_policy_package/role.py
+++ policy-packages/rbac_rucio_policy_package/role.py
@@ -1,30 +1,12 @@
 roles = [
     {
-        "name": "admin",
-        "description": "Admin role with full access to all resources.",
-        "permissions": [
-            {
-                "scope": "*",
-                "action": "rw",
-            },
-        ],
-    },
-    {
         "name": "data-scientist",
         "description": "Data scientist role with read access to data scope and archived content.",
-        "permissions": [
-            {
-                "scope": "data",
-                "action": "r-",
-            },
-            {
-                "scope": "archive*",
-                "action": "r-",
-            },
-            {
-                "scope": "atlas",
-                "action": "rw",
-            }
-        ],
+    },
+    {
+        "name": "superuser",
+        "description": "Full read access to all scopes.",
     },
 ]
+
+default_roles = []  # Default roles assigned to new users
