f24ce6e9da26161908e2e63a360362bbf67c8906
Hugo Demule <hugo.jean.roger.demule@cern.ch>
2026-09-25 16:33:46 +0200

    feat(Policies): updated Policy Package for 'rule' permission checks

--- policy-packages/rbac_rucio_policy_package/permission.py
+++ policy-packages/rbac_rucio_policy_package/permission.py
@@ -49,6 +49,7 @@ def has_permission(issuer: "InternalAccount", action: str, kwargs: dict[str, Any
         'delete_metadata': perm_delete_metadata,
         'get_request_by_did': perm_get_request_by_did,
         'get_request_history_by_did': perm_get_request_by_did,  # The request history of a DID is as sensitive as its current request
+        'list_replication_rule_history': perm_list_replication_rule_history,
         'list_replication_rule_full_history': perm_list_replication_rule_full_history,
         'get_replication_rule': perm_get_replication_rule,
         'examine_replication_rule': perm_examine_replication_rule,
@@ -308,6 +309,17 @@ def perm_get_request_by_did(issuer: "InternalAccount", kwargs: dict[str, Any], s
     """
     Checks if an account can get the transfer request of a DID.
 
+    :param issuer: Account identifier which issues the command.
+    :param kwargs: List of arguments for the action.
+    :param session: The DB session to use
+    :returns: True if account is allowed, otherwise False
+    """
+    return _can_read_in_scope(issuer=issuer, scope=kwargs.get('scope'), session=session)
+
+
+def perm_list_replication_rule_history(issuer: "InternalAccount", kwargs: dict[str, Any], session: "Session") -> bool:
+    """
+    Checks if an account can list the history of a replication rule.
     :param issuer: Account identifier which issues the command.
     :param kwargs: List of arguments for the action.
     :param session: The DB session to use
